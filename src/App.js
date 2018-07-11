4904101f63dfb6838e06189d2fcd3857b74f2386
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-07-11 23:22:44 +0200

    add state into App component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import UserInput from './UserInput/UserInput.js';
 import UserOutput from './UserOutput/UserOutput.js';
 
 class App extends Component {
-  render() {
+state = {
+  users:[
+    {name:'Paweł', city:'Szczecin'},
+    {name:'Michał', city:'Kraków'},
+  ]
+}
+
+render() {
     return (
       <div className="App">
         <UserInput />
-        <UserOutput name="Paweł" city="Szczecin"/>
+        <UserOutput name={this.state.users[0].name} city={this.state.users[0].city}/>
+        <UserOutput name={this.state.users[1].name} city={this.state.users[1].city}/>
       </div>
     );
   }
