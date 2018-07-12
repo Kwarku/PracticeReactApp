3618da908bf6790105c8ad88909d98c8ad475404
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-07-12 20:09:24 +0200

    one input and his handler method

--- src/App.js
+++ src/App.js
@@ -1,49 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserInput from './UserInput/UserInput.js';
-import UserOutput from './UserOutput/UserOutput.js';
 
 class App extends Component {
-  state = {
-    users: [
-      { name: 'Paweł', city: 'Szczecin' },
-      { name: 'Michał', city: 'Kraków' },
-    ]
-  }
-  changeCityHanlder = (cityName) => {
-    this.setState({
-      users: [
-        { name: 'Paweł', city: 'Szczecin' },
-        { name: 'Michał', city: cityName },
-      ]
-    })
-  }
+state ={
+  
+}
 
-  dynamicNameChange = (event) => {
-    this.setState({
-      users: [
-        { name: event.target.value, city: 'Szczecin' },
-        { name: 'Michał', city: 'Kraków' },
-      ]
-    })
-  }
+changeParagraphTextHanlder = (event) => {
+  let tempText = event.target.value;
+  this.setState({
+    text: tempText,
+    textSize : tempText.length 
+  })
+}
 
   render() {
     return (
       <div className="App">
-        <UserInput normalName={this.state.users[0].name} change={this.dynamicNameChange} />
-        <UserOutput
-          name={this.state.users[0].name}
-          city={this.state.users[0].city} />
-
-        <UserOutput
-          name={this.state.users[1].name}
-          city={this.state.users[1].city} />
-
-        <br />
-        <button
-          onClick={this.changeCityHanlder.bind(this, "Kołobrzeg")}
-        >Change City </button>
+        <input onChange={this.changeParagraphTextHanlder} value={this.state.text} type="text" />
+        <p>{this.state.textSize}</p>
       </div>
     );
   }
